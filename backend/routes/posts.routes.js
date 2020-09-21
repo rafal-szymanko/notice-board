47cf2d982619847e91e13462c3c294a54db48cf0
rafal-szymanko <61496255+rafal-szymanko@users.noreply.github.com>
2020-09-21 10:58:47 +0200

    Add validation from server side

--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -34,8 +34,6 @@ router.post('/post/add', async (req, res,) => {
   const {author, title, text } = req.fields;
   const file = req.files.photo;
 
-
-
   function validateEmail(author) {
     // eslint-disable-next-line no-useless-escape
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -43,15 +41,21 @@ router.post('/post/add', async (req, res,) => {
   }
 
   if (title && author && validateEmail(author) && text) {
-    try {
-      const newPost = new Post(
-        {...req.fields, photo: file ? file.path.split('/').slice(-1)[0] : null});    
-      await newPost.save();
-      res.json(await Post.find());
+    if(title.length >= 10 && text.length >= 20) {
+      try {
+        const newPost = new Post(
+          {...req.fields, photo: file ? file.path.split('/').slice(-1)[0] : null});    
+        await newPost.save();
+        res.json(await Post.find());
+      }
+      catch(err) {
+        res.status(500).json({message: err});
+      }
+    } else {
+      throw new Error('Check number of characters');
     }
-    catch(err) {
-      res.status(500).json({message: err});
-    }
+  } else {
+    throw new Error('Wrong input!');
   }
 });
 
@@ -66,16 +70,21 @@ router.put('/post/:id/edit', async (req, res,) => {
     return re.test(String(author).toLowerCase());
   }
 
-  if (title && author && validateEmail(author) && text) {
-    try {
-      console.log(req.fields);
-      const find = await Post.findOneAndUpdate({_id: req.params.id}, {...req.fields, photo: file ? file.path.split('/').slice(-1)[0] : photo}, {returnOriginal: false});
-      console.log(find);
-      res.json(await Post.find());
+  if (title && validateEmail(author) && text) {
+    if(title.length >= 10 && text.length >= 20) {
+      try {
+        const find = await Post.findOneAndUpdate({_id: req.params.id}, {...req.fields, photo: file ? file.path.split('/').slice(-1)[0] : photo}, {returnOriginal: false});
+        console.log(find);
+        res.json(await Post.find());
+      }
+      catch(err) {
+        res.status(500).json({message: err});
+      }
+    } else {
+      throw new Error('Check number of characters');
     }
-    catch(err) {
-      res.status(500).json({message: err});
-    }
+  } else {
+    throw new Error('Wrong input!');
   }
 });
 
